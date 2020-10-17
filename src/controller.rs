149b90502bf94ad2a748719b2ecf9c3402b6606d
Robert Swain <robert.swain@gmail.com>
2020-10-17 19:56:13 +0200

    controller: Overhaul for impulses and forces
    'Simulate' with a fixed time step. Handle input events that happened during
    that fixed time step. This works if the game loop proceeds faster than the
    simulation time step but could break down if the frame rate is below 60fps.
    Ideally events would have timestamps to correlate them with the physics time
    step to which they belong.
    
    A desired velocity in x,z is calculated based on input events and configured
    walk / run speed. If jump was just pressed, the desired y velocity is set to
    the jump speed. The difference between the current velocity in x,z and the
    desired velocity is calculated (to excluding velocity due to falling, which is
    the responsibility of the physics engine, not input). The impulse (change in
    momentum) is calculated by multiplying by the mass of the body. The force (rate
    of change of momentum) is calculated by dividing the impulse by the time step
    (as the force is only active for a single time step.) If we were jumping, we
    apply gravity to the y-component based on the previous y-velocity, else we just
    use the desired velocity as-is and calculate the translation by multiplying by
    the time step. Then we reset the input state because all inputs were handled
    for a single time step of the simulation.

--- src/controller.rs
+++ src/controller.rs
@@ -38,6 +38,16 @@ impl Plugin for CharacterControllerPlugin {
     }
 }
 
+#[derive(Default)]
+pub struct InputState {
+    pub forward: bool,
+    pub backward: bool,
+    pub left: bool,
+    pub right: bool,
+    pub run: bool,
+    pub jump: bool,
+}
+
 pub struct CharacterController {
     pub input_map: InputMap,
     pub fly: bool,
@@ -46,6 +56,9 @@ pub struct CharacterController {
     pub jump_speed: f32,
     pub velocity: Vec3,
     pub jumping: bool,
+    pub dt: f32,
+    pub sim_to_render: f32,
+    pub input_state: InputState,
 }
 
 impl Default for CharacterController {
@@ -58,6 +71,9 @@ impl Default for CharacterController {
             jump_speed: 6.0,
             velocity: Vec3::zero(),
             jumping: false,
+            dt: 1.0 / 60.0,
+            sim_to_render: 0.0,
+            input_state: InputState::default(),
         }
     }
 }
@@ -123,25 +139,56 @@ pub fn controller_to_look_direction(world: &mut World, resources: &mut Resources
 }
 
 pub fn input_to_events(
-    controller_to_look: Res<ControllerToLook>,
     time: Res<Time>,
+    controller_to_look: Res<ControllerToLook>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut events: ResMut<Events<TranslationEvent>>,
-    mut controller_query: Query<(Entity, &mut CharacterController)>,
+    mut translation_events: ResMut<Events<TranslationEvent>>,
+    mut impulse_events: ResMut<Events<ImpulseEvent>>,
+    mut force_events: ResMut<Events<ForceEvent>>,
+    mut controller_query: Query<(Entity, &Mass, &mut CharacterController)>,
     look_direction_query: Query<&LookDirection>,
 ) {
-    for (entity, mut controller) in &mut controller_query.iter() {
+    let xz = Vec3::new(1.0, 0.0, 1.0);
+    for (entity, mass, mut controller) in &mut controller_query.iter() {
+        controller.sim_to_render += time.delta_seconds;
+
+        if keyboard_input.pressed(controller.input_map.key_forward) {
+            controller.input_state.forward = true;
+        }
+        if keyboard_input.pressed(controller.input_map.key_backward) {
+            controller.input_state.backward = true;
+        }
+        if keyboard_input.pressed(controller.input_map.key_right) {
+            controller.input_state.right = true;
+        }
+        if keyboard_input.pressed(controller.input_map.key_left) {
+            controller.input_state.left = true;
+        }
+        if keyboard_input.pressed(controller.input_map.key_run) {
+            controller.input_state.run = true;
+        }
+        if keyboard_input.just_pressed(controller.input_map.key_jump) {
+            controller.input_state.jump = true;
+        }
+
+        if controller.sim_to_render < controller.dt {
+            continue;
+        }
+        // Calculate the remaining simulation to render time after all
+        // simulation steps were taken
+        controller.sim_to_render %= controller.dt;
+
         let look_entity = controller_to_look
             .get(&entity)
             .expect("Failed to look up LookDirection");
         let look = look_direction_query
             .get::<LookDirection>(*look_entity)
             .expect("Failed to get LookDirection from Entity");
+
         // Calculate forward / right / up vectors
         let (forward, right, _up) = if controller.fly {
             (look.forward, look.right, look.up)
         } else {
-            let xz = Vec3::new(1.0, 0.0, 1.0);
             (
                 (look.forward * xz).normalize(),
                 (look.right * xz).normalize(),
@@ -149,48 +196,70 @@ pub fn input_to_events(
             )
         };
 
-        // Calculate velocity based on input
-        let mut velocity = Vec3::zero();
-        if keyboard_input.pressed(controller.input_map.key_forward) {
-            velocity += forward;
+        // Calculate the desired velocity based on input
+        let mut desired_velocity = Vec3::zero();
+        if controller.input_state.forward {
+            desired_velocity += forward;
         }
-        if keyboard_input.pressed(controller.input_map.key_backward) {
-            velocity -= forward;
+        if controller.input_state.backward {
+            desired_velocity -= forward;
         }
-        if keyboard_input.pressed(controller.input_map.key_right) {
-            velocity += right;
+        if controller.input_state.right {
+            desired_velocity += right;
         }
-        if keyboard_input.pressed(controller.input_map.key_left) {
-            velocity -= right;
-        }
-
-        if velocity.length_squared() > 1E-6 {
-            controller.velocity = velocity.normalize() * Vec3::new(1.0, 0.0, 1.0)
-                + controller.velocity * Vec3::unit_y();
-        } else {
-            controller.velocity *= Vec3::new(0.5, 1.0, 0.5);
+        if controller.input_state.left {
+            desired_velocity -= right;
         }
 
-        let speed = if keyboard_input.pressed(controller.input_map.key_run) {
+        // Limit x/z velocity to walk/run speed
+        let speed = if controller.input_state.run {
             controller.run_speed
         } else {
             controller.walk_speed
         };
+        desired_velocity = if desired_velocity.length_squared() > 1E-6 {
+            desired_velocity.normalize() * speed
+        } else {
+            // No input - apply damping to the x/z of the current velocity
+            controller.velocity * 0.5 * xz
+        };
 
-        *controller.velocity.y_mut() = if controller.jumping {
-            (-9.81f32).mul_add(time.delta_seconds, controller.velocity.y())
-        } else if keyboard_input.just_pressed(controller.input_map.key_jump) {
+        // Handle jumping
+        let was_jumping = controller.jumping;
+        *desired_velocity.y_mut() = if controller.input_state.jump {
             controller.jumping = true;
             controller.jump_speed
         } else {
             0.0
         };
 
-        let translation = (controller.velocity * Vec3::new(1.0, 0.0, 1.0) * speed
-            + controller.velocity * Vec3::unit_y())
-            * time.delta_seconds;
-        if translation.length_squared() > 1E-6 {
-            events.send(TranslationEvent::new(&translation));
+        // Calculate impulse - the desired momentum change for the time period
+        let delta_velocity = desired_velocity - controller.velocity * xz;
+        let impulse = delta_velocity * mass.mass;
+        if impulse.length_squared() > 1E-6 {
+            impulse_events.send(ImpulseEvent::new(&impulse));
         }
+
+        // Calculate force - the desired rate of change of momentum for the time period
+        let force = impulse / controller.dt;
+        if force.length_squared() > 1E-6 {
+            force_events.send(ForceEvent::new(&force));
+        }
+
+        *controller.velocity.x_mut() = desired_velocity.x();
+        *controller.velocity.z_mut() = desired_velocity.z();
+        *controller.velocity.y_mut() = if was_jumping {
+            // Apply gravity for kinematic simulation
+            (-9.81f32).mul_add(controller.dt, controller.velocity.y())
+        } else {
+            desired_velocity.y()
+        };
+
+        let translation = controller.velocity * controller.dt;
+        if translation.length_squared() > 1E-6 {
+            translation_events.send(TranslationEvent::new(&translation));
+        }
+
+        controller.input_state = InputState::default();
     }
 }
