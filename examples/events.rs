22944e49db682ee2adbd8715528d1e4436ba8fb7
Robert Swain <robert.swain@gmail.com>
2020-10-17 20:07:11 +0200

    examples/events: Add impulse and force events

--- examples/events.rs
+++ examples/events.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 use bevy_prototype_character_controller::events::{
-    LookDeltaEvent, LookEvent, PitchEvent, TranslationEvent, YawEvent,
+    ForceEvent, ImpulseEvent, LookDeltaEvent, LookEvent, PitchEvent, TranslationEvent, YawEvent,
 };
 
 // Take a look at example_utils/utils.rs for details!
@@ -19,12 +19,20 @@ fn main() {
 fn print_controller_events(
     mut reader: ResMut<ControllerEvents>,
     translations: Res<Events<TranslationEvent>>,
+    impulses: Res<Events<ImpulseEvent>>,
+    forces: Res<Events<ForceEvent>>,
     pitches: Res<Events<PitchEvent>>,
     yaws: Res<Events<YawEvent>>,
     looks: Res<Events<LookEvent>>,
     look_deltas: Res<Events<LookDeltaEvent>>,
 ) {
     for event in reader.translations.iter(&translations) {
+        println!("{:?}", event);
+    }
+    for event in reader.impulses.iter(&impulses) {
+        println!("{:?}", event);
+    }
+    for event in reader.forces.iter(&forces) {
         println!("{:?}", event);
     }
     for event in reader.pitches.iter(&pitches) {
